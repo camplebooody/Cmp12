f09acc3e15444f7bebcf40cd1568f0cf5db77aac
3 <)>
2016-04-15 17:41:58 +0300

    Change2
    123

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,12 +2,13 @@
 //
 
 #include "stdafx.h"
+#include "iostream"
 
-
+using namespace std;
 int _tmain(int argc, _TCHAR* argv[])
 {
 	cout << "hello world!" << endl;
-	cout << "i'm stupid man" << endl;
+	cout << "i'm not stupid man" << endl;
 	return 0;
 }
 
