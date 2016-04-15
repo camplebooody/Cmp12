5fce6d8fbb89328e9e18b3fd92d4f43dc9ef8192
3 <)>
2016-04-15 17:16:41 +0300

    lalalal
    123

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,12 @@
+// ConsoleApplication1.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "hello world!" << endl;
+	return 0;
+}
+
